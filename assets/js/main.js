c5ea3b066a296b07928f47890499f3da57acbac8
Deus Jeraldy <deusjeraldy@gmail.com>
2018-01-16 12:49:59 +0300

    removed calender

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function ($) {
     );
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar(".calendar", "jeraldy");
+    //GitHubCalendar(".calendar", "jeraldy");
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "jeraldy", selector: "#ghfeed" });
 
